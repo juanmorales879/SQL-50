d1ce420bf6ed9dd3d879ab7482209c817d36044a
juanmorales879 <juanmorales.marcano@gmail.com>
2025-10-13 19:28:25 -0700

    Update SQL50.sql

--- SQL50.sql
+++ SQL50.sql
@@ -387,3 +387,13 @@ WHERE
       product_id
   )
 
+-- Last person to fit in the bus
+
+# Write your MySQL query statement below
+SELECT 
+    q1.person_name
+FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1
